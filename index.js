0c98609b9dd785be4307b21ae65934b5a81b3234
refrain62 <nao_h13@hotmail.com>
2024-01-28 17:01:09 +0900

    publicフォルダを公開フォルダに変更

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => res.send('Hello world"'));
+// pulibc フォルダ配下を公開するように変更
+//app.get('/', (req, res) => res.send('Hello world"'));
+app.use(express.static('public'));
 
 app.listen(port, () => console.log(`Express app listening on port ${port}`));
